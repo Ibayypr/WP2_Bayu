5665dbf84accb51e113047dc1550761dd7ebd75e
WP2_Bayu <prasetyobayu756@gmail.com>
2023-10-19 20:31:52 +0700

    menambahkan file contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function INDEX()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "nama saya bayu prasetyo saya tinggal di bekasi
+        olahraga yang saya sukai adalah futsal";
+    }
+}
